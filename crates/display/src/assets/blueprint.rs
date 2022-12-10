af216232b65aa1761e0f31ce7658205442be2f9a
zebi <hugo.gardes@epitech.eu>
2022-12-10 15:24:59 +0100

    start drag system

--- crates/display/src/assets/blueprint.rs
+++ crates/display/src/assets/blueprint.rs
@@ -1,35 +1,38 @@
-use bevy::ecs::system::EntityCommands;
+use bevy::ecs::entity;
+use bevy::{ecs::system::EntityCommands};
 use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct BlueprintBase;
 
-#[derive(Resource)]
+#[derive(Resource, Clone)]
 pub struct Assets {
     pub font: Handle<Font>,
     pub icon: Handle<Image>,
 }
 
 #[derive(Component)]
-struct Object {
-    pub asset: &Assets,
+pub struct Object {
+    pub asset: Assets,
     pub name: String,
     pub description: String,
     pub is_dragable: bool,
+    pub is_pressed: bool,
 }
 
 impl Object {
-    pub fn new(asset: &Assets, name: String, description: String, is_dragable: bool) -> Self {
+    pub fn new(asset: &Assets, name: String, description: String, is_dragable: bool, is_pressed: bool) -> Self {
         Self {
-            asset,
+            asset: asset.clone().into(),
             name,
             description,
             is_dragable,
+            is_pressed,
         }
     }
 
     pub fn spawn(&self, mut commands: EntityCommands) {
-        commands.insert(ImageBundle {
+        commands.insert(ButtonBundle  {
             style: Style {
                 align_self: AlignSelf::Center,
                 ..default()
@@ -37,33 +40,41 @@ impl Object {
             transform: Transform::from_scale(Vec3::new(1., 1., 1.)),
             image: self.asset.icon.clone().into(),
             ..default()
-        });
-        self.spawn_text(commands, self.asset);
-    }
-
-    pub fn spawn_text(&self, mut commands: EntityCommands, assets: &Assets) {
-        commands.insert((
-            TextBundle::from_sections([
+        }).with_children(|parent| {
+            parent.spawn(TextBundle::from_sections([
                 TextSection::new(
                     self.name.clone(),
                     TextStyle {
                         font: self.asset.font.clone(),
-                        font_size: 60.0,
-                        color: Color::BLACK,
+                        font_size: 20.0,
+                        color: Color::RED,
                     },
                 ),
-            ]),
-            TextBundle::from_sections([
                 TextSection::new(
                     self.description.clone(),
                     TextStyle {
                         font: self.asset.font.clone(),
-                        font_size: 60.0,
-                        color: Color::GREEN,
+                        font_size: 15.0,
+                        color: Color::BLUE,
                     },
                 ),
-            ]),
-        ));
+                ])
+            );
+        });
+    }
+}
+
+pub fn drag(mut commands: Commands, buttons: Res<Input<MouseButton>>, mut query: Query<(Entity, With<Object>, &mut Object)>) {
+    for (mut _entity, _, mut _object) in &mut query {
+        if buttons.pressed(MouseButton::Left) {
+            if _object.is_dragable {
+                println!("Drag");
+                commands.get_entity(_entity);
+            }
+        } else if _object.is_pressed {
+            println!("Release");
+            _object.is_pressed = false;
+        }
     }
 }
 
@@ -77,6 +88,12 @@ pub fn load_assets(mut commands: Commands, assets: Res<AssetServer>) {
 }
 
 pub fn spawn_blueprint(mut commands: EntityCommands, _assets: &Assets) {
+    let obj = Object::new(_assets, "Button 1".to_string(), "First button".to_string(), true, false);
+    let obj2 = Object::new(_assets, "Button 2".to_string(), "Second button".to_string(), true, false);
+    commands.with_children(|parent| obj.spawn(parent.spawn_empty()));
+    commands.with_children(|parent| obj2.spawn(parent.spawn_empty()));
+    commands.insert(obj);
+    commands.insert(obj2);
     commands.insert(ImageBundle {
         style: Style {
             align_self: AlignSelf::Center,
@@ -92,11 +109,6 @@ pub fn spawn_blueprint(mut commands: EntityCommands, _assets: &Assets) {
 pub fn spawn_box(mut commands: EntityCommands, _assets: &Assets, windows: Res<Windows>) {
     let window = windows.get_primary().unwrap();
     commands.insert(NodeBundle {
-        // sprite: Sprite {
-        //     color: Color::rgb(0.25, 0.25, 0.75),
-        //     custom_size: Some(Vec2::new(50.0, 100.0)),
-        //     ..default()
-        // },
         style: Style {
             position: UiRect::new(Val::Px(0.), Val::Px(0.), Val::Px(0.), Val::Px(0.)),
             position_type: PositionType::Absolute,
@@ -107,15 +119,3 @@ pub fn spawn_box(mut commands: EntityCommands, _assets: &Assets, windows: Res<Wi
         ..default()
     });
 }
-
-// pub fn spawn_icon(mut commands: EntityCommands, assets: &Assets) {
-//     commands.insert(ImageBundle {
-//         style: Style {
-//             align_self: AlignSelf::Center,
-//             ..default()
-//         },
-//         transform: Transform::from_scale(Vec3::new(0.5, 0.5, 1.0)),
-//         image: assets.icon.clone().into(),
-//         ..default()
-//     });
-// }
