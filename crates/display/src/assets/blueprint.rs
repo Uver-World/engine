eff66ea5f78aa7a15ad22db96d039a2193fd350a
zebi <hugo.gardes@epitech.eu>
2022-12-10 17:56:48 +0100

    wont move

--- crates/display/src/assets/blueprint.rs
+++ crates/display/src/assets/blueprint.rs
@@ -1,4 +1,4 @@
-use bevy::ecs::entity;
+use bevy::render::camera::RenderTarget;
 use bevy::{ecs::system::EntityCommands};
 use bevy::prelude::*;
 
@@ -18,29 +18,41 @@ pub struct Object {
     pub description: String,
     pub is_dragable: bool,
     pub is_pressed: bool,
+    pub pos: Vec2,
+    pub bund: ButtonBundle
 }
 
 impl Object {
-    pub fn new(asset: &Assets, name: String, description: String, is_dragable: bool, is_pressed: bool) -> Self {
+    pub fn new(asset: &Assets, name: String, description: String, is_dragable: bool, is_pressed: bool, pos: Vec2) -> Self {
         Self {
             asset: asset.clone().into(),
             name,
             description,
             is_dragable,
             is_pressed,
+            pos,
+            bund: ButtonBundle {
+                style: Style {
+                    align_self: AlignSelf::Center,
+                    position_type: PositionType::Absolute,
+                    position: UiRect {
+                        left: Val::Px(pos.x),
+                        top: Val::Px(pos.y),
+                        ..default()
+                    },
+                    ..default()
+                },
+                transform: Transform::from_scale(Vec3::new(1., 1., 1.)),
+                image: asset.icon.clone().into(),
+                ..default()
+            }
         }
     }
 
     pub fn spawn(&self, mut commands: EntityCommands) {
-        commands.insert(ButtonBundle  {
-            style: Style {
-                align_self: AlignSelf::Center,
-                ..default()
-            },
-            transform: Transform::from_scale(Vec3::new(1., 1., 1.)),
-            image: self.asset.icon.clone().into(),
-            ..default()
-        }).with_children(|parent| {
+        commands.insert(
+            self.bund.clone()
+        ).with_children(|parent| {
             parent.spawn(TextBundle::from_sections([
                 TextSection::new(
                     self.name.clone(),
@@ -64,15 +76,38 @@ impl Object {
     }
 }
 
-pub fn drag(mut commands: Commands, buttons: Res<Input<MouseButton>>, mut query: Query<(Entity, With<Object>, &mut Object)>) {
+pub fn drag(
+    mut _commands: Commands,
+    q_camera: Query<(&Camera, &GlobalTransform),With<Camera2d>>,
+    buttons: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mut query: Query<(Entity, With<Object>, &mut Object)>,
+    mut query2: Query<&mut Transform, With<Object>>
+) {
     for (mut _entity, _, mut _object) in &mut query {
         if buttons.pressed(MouseButton::Left) {
             if _object.is_dragable {
-                println!("Drag");
-                commands.get_entity(_entity);
+                let mut transform = query2.single_mut();
+                let (camera, camera_transform) = q_camera.single();
+                let wnd = if let RenderTarget::Window(id) = camera.target {
+                    windows.get(id).unwrap()
+                } else {
+                    windows.get_primary().unwrap()
+                };
+                if let Some(screen_pos) = wnd.cursor_position() {
+                    let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+                    let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+                    let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+                    let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+                    let world_pos: Vec2 = world_pos.truncate();
+                    _object.pos = Vec2::new(world_pos.x, world_pos.y);
+                    println!("transform base = {:?}", transform.translation);
+                    transform.translation.x = _object.pos.x;
+                    transform.translation.y = _object.pos.y;
+                    println!("Drag to {:?}", _object.pos);
+                }
             }
-        } else if _object.is_pressed {
-            println!("Release");
+        } else if buttons.just_released(MouseButton::Left) {
             _object.is_pressed = false;
         }
     }
@@ -88,12 +123,12 @@ pub fn load_assets(mut commands: Commands, assets: Res<AssetServer>) {
 }
 
 pub fn spawn_blueprint(mut commands: EntityCommands, _assets: &Assets) {
-    let obj = Object::new(_assets, "Button 1".to_string(), "First button".to_string(), true, false);
-    let obj2 = Object::new(_assets, "Button 2".to_string(), "Second button".to_string(), true, false);
+    let obj = Object::new(_assets, "Button 1".to_string(), "First button".to_string(), true, false, Vec2::new(0., 0.));
+    // let obj2 = Object::new(_assets, "Button 2".to_string(), "Second button".to_string(), true, false, Vec2::new(500., 100.));
     commands.with_children(|parent| obj.spawn(parent.spawn_empty()));
-    commands.with_children(|parent| obj2.spawn(parent.spawn_empty()));
+    // commands.with_children(|parent| obj2.spawn(parent.spawn_empty()));
     commands.insert(obj);
-    commands.insert(obj2);
+    // commands.insert(obj2);
     commands.insert(ImageBundle {
         style: Style {
             align_self: AlignSelf::Center,
