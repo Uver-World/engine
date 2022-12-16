836d6f6df873896feadc71ec765b310cc18a1775
Guismus <Guillaume.boudaille@epitech.eu>
2022-12-16 06:47:50 +0100

    Validate button add instead of + button

--- crates/display/src/assets/button_blueprint.rs
+++ crates/display/src/assets/button_blueprint.rs
@@ -9,9 +9,10 @@ use crate::ClientDisplay;
 
 pub fn button_system(
     mut interaction_query: Query<
-        (&Interaction, &Children, &mut Style, &mut Transform),
+        (&Interaction, &Children, &mut Transform),
         (Changed<Interaction>, With<Button>),
     >,
+    mut button_query: Query<(&Children, &mut Style), With<Button>>,
     mut text_query: Query<&mut Text>,
     mut client_display: ResMut<ClientDisplay>,
     assets: Res<blueprint::Assets>,
@@ -19,29 +20,39 @@ pub fn button_system(
     mut entity: Query<Entity, With<Blueprint>>,
     ass: Res<AssetServer>,
 ) {
-    for (interaction, children, mut style, _transform) in &mut interaction_query {
+    for (interaction, children, _transform) in &mut interaction_query {
         let mut text = text_query.get_mut(children[0]).unwrap();
         match *interaction {
             Interaction::Clicked => {
                 text.sections[0].style.color = Color::rgb_u8(0, 0, 0).into();
                 if text.sections[0].value == "+" {
                     client_display.is_toggled = true;
-                    let mut i = 1.0;
-                    let mut temp = Val::Percent(i);
-                    while temp != style.position.left {
-                        i += 11.;
-                        temp = Val::Percent(i);
-                    }
-                    for entity in &mut entity {
-                        commands.entity(entity).with_children(|parent| {
-                            spawn_blueprint(parent.spawn_empty(), &assets, Vec2 { x: i, y: 87. })
-                        });
-                    }
-                    i += 11.;
-                    temp = Val::Percent(i);
-                    style.position.left = temp;
                 }
-                if text.sections[0].value == "Valider" {}
+                if text.sections[0].value == "Valider" {
+                    for (child, mut sty) in &mut button_query {
+                        let text_button = text_query.get_mut(child[0]).unwrap();
+                        if text_button.sections[0].value == "+" {
+                            let mut i = 1.0;
+                            let mut temp = Val::Percent(i);
+                            while temp != sty.position.left {
+                                i += 11.;
+                                temp = Val::Percent(i);
+                            }
+                            for entity in &mut entity {
+                                commands.entity(entity).with_children(|parent| {
+                                    spawn_blueprint(
+                                        parent.spawn_empty(),
+                                        &assets,
+                                        Vec2 { x: i, y: 87. },
+                                    )
+                                });
+                            }
+                            i += 11.;
+                            temp = Val::Percent(i);
+                            sty.position.left = temp;
+                        }
+                    }
+                }
             }
             Interaction::Hovered => {
                 text.sections[0].style.color = Color::rgb_u8(100, 100, 100).into()
