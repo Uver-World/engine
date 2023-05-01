e43d8dd7453dd9bac0bfe7c79499f1a53a07046e
Hugo-Gardes <hugo.gardes@epitech.eu>
2023-05-01 18:02:02 +0200

    test drag

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,63 +1,50 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
-
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
-}
-
-function openDocs() {
-    Neutralino.os.open("https://neutralino.js.org/docs");
-}
-
-function openTutorial() {
-    Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
-}
-
-function setTray() {
-    if(NL_MODE != "window") {
-        console.log("INFO: Tray menu is only available in the window mode.");
-        return;
-    }
-    let tray = {
-        icon: "/resources/icons/trayIcon.png",
-        menuItems: [
-            {id: "VERSION", text: "Get version"},
-            {id: "SEP", text: "-"},
-            {id: "QUIT", text: "Quit"}
-        ]
-    };
-    Neutralino.os.setTray(tray);
-}
-
-function onTrayMenuItemClicked(event) {
-    switch(event.detail.id) {
-        case "VERSION":
-            Neutralino.os.showMessageBox("Version information",
-                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-            break;
-        case "QUIT":
-            Neutralino.app.exit();
-            break;
-    }
-}
-
-function onWindowClose() {
-    Neutralino.app.exit();
-}
-
 Neutralino.init();
 
-Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
-Neutralino.events.on("windowClose", onWindowClose);
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
-    setTray();
+let squareX = 50;
+let squareY = 50;
+let dragging = false;
+let dragStartX;
+let dragStartY;
+
+function drawSquare() {
+    context.fillStyle = "#FF0000";
+    context.fillRect(squareX, squareY, 100, 100);
 }
 
-showInfo();
+canvas.addEventListener("mousedown", function (event) {
+    let mouseX = event.clientX - canvas.offsetLeft;
+    let mouseY = event.clientY - canvas.offsetTop;
+    if (mouseX >= squareX && mouseX <= squareX + 100 && mouseY >= squareY && mouseY <= squareY + 100) {
+        dragging = true;
+        dragStartX = mouseX - squareX;
+        dragStartY = mouseY - squareY;
+    }
+});
+
+canvas.addEventListener("mousemove", function (event) {
+    if (dragging) {
+        let mouseX = event.clientX - canvas.offsetLeft;
+        let mouseY = event.clientY - canvas.offsetTop;
+        squareX = mouseX - dragStartX;
+        squareY = mouseY - dragStartY;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawSquare();
+    }
+});
+
+canvas.addEventListener("mouseup", function (event) {
+    dragging = false;
+});
+
+Neutralino.window.setTitle("Ma fenêtre NeutralinoJS");
+Neutralino.window.setSize(300, 300);
+Neutralino.events.on("load", function () {
+    drawSquare();
+});
+Neutralino.events.on('windowClose', () => {
+    Neutralino.app.exit();
+});
+// getUsername();
