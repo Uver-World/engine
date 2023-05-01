b837cefe9eebef17c0b2b9e36db294593bb61daf
Hugo-Gardes <hugo.gardes@epitech.eu>
2023-05-01 18:31:34 +0200

    square become object and spawn another square at initial pos

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,46 +3,64 @@ Neutralino.init();
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-let squareX = 50;
-let squareY = 50;
-let dragging = false;
-let dragStartX;
-let dragStartY;
+let square = { x: 50, y: 50, dragging: false, dragStartX: 0, dragStartY: 0 };
+let squares = [];
 
-function drawSquare() {
+function drawSquare(x, y) {
     context.fillStyle = "#FF0000";
-    context.fillRect(squareX, squareY, 100, 100);
+    context.fillRect(x, y, 100, 100);
 }
 
 canvas.addEventListener("mousedown", function (event) {
     let mouseX = event.clientX - canvas.offsetLeft;
     let mouseY = event.clientY - canvas.offsetTop;
-    if (mouseX >= squareX && mouseX <= squareX + 100 && mouseY >= squareY && mouseY <= squareY + 100) {
-        dragging = true;
-        dragStartX = mouseX - squareX;
-        dragStartY = mouseY - squareY;
+    for (let i = squares.length - 1; i >= 0; i--) {
+        console.log("mouseX: " + mouseX + " mouseY: " + mouseY);
+        console.log("x: " + squares[i].x + " y: " + squares[i].y);
+        if (mouseX >= squares[i].x && mouseX <= squares[i].x + 100 && mouseY >= squares[i].y && mouseY <= squares[i].y + 100) {
+            squares[i].dragging = true;
+            console.log("dragging");
+            squares[i].dragStartX = mouseX - squares[i].x;
+            squares[i].dragStartY = mouseY - squares[i].y;
+            break;
+        }
     }
 });
 
 canvas.addEventListener("mousemove", function (event) {
-    if (dragging) {
-        let mouseX = event.clientX - canvas.offsetLeft;
-        let mouseY = event.clientY - canvas.offsetTop;
-        squareX = mouseX - dragStartX;
-        squareY = mouseY - dragStartY;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        drawSquare();
+    for (let i = squares.length - 1; i >= 0; i--) {
+        if (squares[i].dragging) {
+            let mouseX = event.clientX - canvas.offsetLeft;
+            let mouseY = event.clientY - canvas.offsetTop;
+            squares[i].x = mouseX - squares[i].dragStartX;
+            squares[i].y = mouseY - squares[i].dragStartY;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            for (let j = 0; j < squares.length; j++) {
+                drawSquare(squares[j].x, squares[j].y);
+            }
+            break;
+        }
     }
 });
 
 canvas.addEventListener("mouseup", function (event) {
-    dragging = false;
+    for (let i = squares.length - 1; i >= 0; i--) {
+        if (squares[i].dragging) {
+            squares[i].dragging = false;
+            let newSquare = Object.assign({}, square);;
+            squares.push(newSquare);
+            drawSquare(newSquare.x, newSquare.y);
+            break;
+        }
+    }
 });
 
 Neutralino.window.setTitle("Ma fenêtre NeutralinoJS");
 Neutralino.window.setSize(300, 300);
 Neutralino.events.on("load", function () {
-    drawSquare();
+    let newSquare = Object.assign({}, square);;
+    squares.push(newSquare)
+    drawSquare(newSquare.x, newSquare.y);
 });
 Neutralino.events.on('windowClose', () => {
     Neutralino.app.exit();
